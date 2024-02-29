b3aaa1d67f09bed56981c9848b04cd4643e27ff8
Thomas Peterson <info@thomas-peterson.de>
2024-02-29 20:54:21 +0100

    fix

---
+++ src/SPC/builder/extension/sdl.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SPC\builder\extension;
+
+use SPC\builder\Extension;
+use SPC\exception\RuntimeException;
+use SPC\store\FileSystem;
+use SPC\util\CustomExt;
+
+#[CustomExt('sdl')]
+class sdl extends Extension
+{
+    /**
+     * @throws RuntimeException
+     */
+    public function patchBeforeBuildconf(): bool
+    {
+        if (file_exists(SOURCE_PATH . DIRECTORY_SEPARATOR . 'php-src' . DIRECTORY_SEPARATOR . 'ext' . DIRECTORY_SEPARATOR . 'sdl')) {
+            return false;
+        }
+        FileSystem::copyDir(SOURCE_PATH . DIRECTORY_SEPARATOR . 'ext-sdl', SOURCE_PATH . DIRECTORY_SEPARATOR . 'php-src' . DIRECTORY_SEPARATOR . 'ext' . DIRECTORY_SEPARATOR . 'sdl');
+        return true;
+    }
+
+    public function patchBeforeConfigure(): bool
+    {
+       // FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/configure', '-lglfw ', '-lglfw3 ');
+        return true;
+    }
+
+    public function getUnixConfigureArg(): string
+    {
+        return '--enable-sdl --with-sdl-dir=' . BUILD_ROOT_PATH;
+    }
+
+    public function getWindowsConfigureArg(): string
+    {
+        return '--enable-sdl=static';
+    }
+}
---
+++ src/SPC/builder/windows/library/sdl.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SPC\builder\windows\library;
+
+use SPC\store\FileSystem;
+
+class sdl extends WindowsLibraryBase
+{
+    public const NAME = 'sdl';
+
+    protected function build(): void
+    {
+        // reset cmake
+        FileSystem::resetDir($this->source_dir . DIRECTORY_SEPARATOR . 'build');
+
+        // start build
+        cmd()->cd($this->source_dir)
+            ->execWithWrapper(
+                $this->builder->makeSimpleWrapper('cmake'),
+                '-B build ' .
+                '-DCMAKE_BUILD_TYPE=Release ' .
+                //'-DSDL_DISABLE_INSTALL=1 '.
+                '-DSDL_DISABLE_INSTALL_DOCS=1'.
+                '-DSDL_TEST_LIBRARY=0'.
+                '-DSDL_STATIC=1 '.
+                '-DSDL_SHARED=1 '.
+                '-DCMAKE_INSTALL_PREFIX=' . BUILD_ROOT_PATH . ' '
+            )
+            ->execWithWrapper(
+                $this->builder->makeSimpleWrapper('cmake'),
+                '--build build --config Release --target install '.
+                "-j{$this->builder->concurrency}"
+            );
+    }
+}
